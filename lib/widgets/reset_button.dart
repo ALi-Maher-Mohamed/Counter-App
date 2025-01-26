c63f241d99c9c8b2f2dcf7be2752f0c9df2d45b1
AliMaher <ali.maher0013@gmail.com>
2025-01-26 22:44:39 +0200

    final touch

--- lib/widgets/reset_button.dart
+++ lib/widgets/reset_button.dart
@@ -1,4 +1,6 @@
+import 'package:counter_application/cubit/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomResetButton extends StatelessWidget {
   const CustomResetButton({
@@ -13,7 +15,9 @@ class CustomResetButton extends StatelessWidget {
         backgroundColor: Colors.orange,
         minimumSize: const Size(150, 50),
       ),
-      onPressed: () {},
+      onPressed: () {
+        BlocProvider.of<CounterCubit>(context).reset();
+      },
       child: const Text(
         'Reset',
         style: TextStyle(
